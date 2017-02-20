bda07a6fe94508786a701aa043f9e8b9ad2f0286
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-02-20 16:22:45 -0500

    config webpack with server

--- source/server.js
+++ source/server.js
@@ -1,14 +1,17 @@
 import http from 'http';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import {
+  renderToString
+}
+from 'react-dom/server';
 
-function requestHandler(request, response){
+function requestHandler(request, response) {
   const html = renderToString(
-  React.DOM.h1(null,hola)
+    React.DOM.h1(null, 'hola')
   );
-
-response.write(html);
-response.end();
+  
+  response.write(html);
+  response.end();
 }
 
 const server = http.createServer(requestHandler);
